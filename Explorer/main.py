23bea7d9a0e50ffaf7815a9c549af3600bb6f254
Kyungwook Nam <nkwook@kaist.ac.kr>
2023-06-06 15:13:04 +0900

    feat: recording q value

--- Explorer/main.py
+++ Explorer/main.py
@@ -2,15 +2,19 @@ import os
 import sys
 import argparse
 
+import wandb
+
 from utils.sweeper import Sweeper
 from utils.helper import make_dir
 from experiment import Experiment
 
 def main(argv):
+
   parser = argparse.ArgumentParser(description="Config file")
   parser.add_argument('--config_file', type=str, default='./configs/catcher.json', help='Configuration file for the chosen model')
   parser.add_argument('--config_idx', type=int, default=1, help='Configuration index')
   parser.add_argument('--slurm_dir', type=str, default='', help='slurm tempory directory')
+  parser.add_argument("--wandb_mode", default="disabled", choices=["online", "offline", "disabled"])
   args = parser.parse_args()
   
   sweeper = Sweeper(args.config_file)
@@ -38,6 +42,16 @@ def main(argv):
   cfg['test_log_path'] = cfg['logs_dir'] + 'result_Test.feather'
   cfg['model_path'] = cfg['logs_dir'] + 'model.pt'
   cfg['cfg_path'] = cfg['logs_dir'] + 'config.json'
+
+  wandb.init(
+    project='qoverestimation',
+    # group="maxmin_catcher", 
+    # entity="ruddnrdl",
+    config=args,
+    mode=args.wandb_mode,
+    name=args.config_file.split('/')[2].split('.')[0] + "/qvaluetest"
+  )
+  # wandb.log()
 
   exp = Experiment(cfg)
   exp.run()
